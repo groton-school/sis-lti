78b26489f8bc49b62f96b9d0224803cd2e88b832
Seth Battis <sbattis@groton.org>
2025-09-26 12:17:01 -0400

    refactor: clarify error message
    Working on #5

--- packages/sis-courses-lti/src/Application/Handlers/LaunchHandler.php
+++ packages/sis-courses-lti/src/Application/Handlers/LaunchHandler.php
@@ -21,21 +21,28 @@ class LaunchHandler implements LaunchHandlerInterface
 
     public function handle(ResponseInterface $response, LtiMessageLaunch $launch): ResponseInterface
     {
-        $sections = array_combine(
-            json_decode($launch->getLaunchData()[LtiConstants::CUSTOM]['section_names']),
-            array_map(
-                fn ($id) => 'https://' . $this->settings->getBlackbaudInstance() . ".myschoolapp.com/app/faculty?override=true#academicclass/{$id}/0/bulletinboard",
-                str_replace('cls-542-', '', explode(',', $launch->getLaunchData()[LtiConstants::CUSTOM]['sis_section_ids']))
-            )
-        );
-        if (count($sections) === 1) {
-            return $this->views->render($response, 'redirect.php', [
-                'redirect' => array_pop($sections)
-            ]);
+        $sectionNames = json_decode($launch->getLaunchData()[LtiConstants::CUSTOM]['section_names']);
+        $sectionIds = explode(',', $launch->getLaunchData()[LtiConstants::CUSTOM]['sis_section_ids']);
+        if (count($sectionNames) == count($sectionIds)) {
+            $sections = array_combine(
+                $sectionNames,
+                array_map(
+                    fn ($id) => 'https://' . $this->settings->getBlackbaudInstance() . ".myschoolapp.com/app/faculty?override=true#academicclass/{$id}/0/bulletinboard",
+                    str_replace('cls-542-', '', $sectionIds)
+                )
+            );
+            if (count($sections) === 1) {
+                return $this->views->render($response, 'redirect.php', [
+                    'redirect' => array_pop($sections)
+                ]);
+            } else {
+                return $this->views->render($response, 'picker.php', [
+                    'sections' => $sections
+                ]);
+            }
         } else {
-            return $this->views->render($response, 'picker.php', [
-                'sections' => $sections
-            ]);
+            $response->getBody()->write("error: unenrolled user");
+            return $response;
         }
     }
 }
