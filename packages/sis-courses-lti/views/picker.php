e1308b03d70c804549447a9c58170df62b59732d
Seth Battis <sbattis@groton.org>
2025-09-27 08:59:41 -0400

    refactor: restyle picker as modal

--- packages/sis-courses-lti/views/picker.php
+++ packages/sis-courses-lti/views/picker.php
@@ -10,29 +10,34 @@
 </head>
 
 <body>
-    <div class="container">
-        <h1>Choose a section</h1>
-        <table>
-            <tbody>
-                <?php
-                foreach ($sections as $name => $url) {
-                    $style = "";
-                    preg_match("/\((.+ )?(RD|OR|YL|GR|LB|DB|PR)( .+)?\)$/", $name, $match);
-                    if (!empty($match[2])) {
-                        $color = strtolower($match[2]);
-                        $style = "style=\"background: var(--$color); color: var(--text-on-$color);\"";
-                    }
-                    ?>
-                    <tr>
-                        <td>
+    <div id="modal" class="modal fade" tabindex="-1" data-bs-keyboard="false" data-bs-backdrop="static">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Choose a section</h5>
+                </div>
+                <div class="modal-body">
+                    <?php
+                    foreach ($sections as $name => $url) {
+                        $style = "";
+                        preg_match("/\((.+ )?(RD|OR|YL|GR|LB|DB|PR)( .+)?\)$/", $name, $match);
+                        if (!empty($match[2])) {
+                            $color = strtolower($match[2]);
+                            $style = "style=\"background: var(--$color); color: var(--text-on-$color);\"";
+                        }
+                        ?>
+                        <div>
                             <a href="<?= $url ?>" class="btn btn-secondary m-3" <?= $style ?> target="_top"><?= $name ?></a>
-                        </td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <script>
+        bootstrap.Modal.getOrCreateInstance('#modal').show()
+    </script>
 </body>
 
 </html>
